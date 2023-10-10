f780985e1d49fe9575fe5bd7862b1fe53996093e
Alice Wu <alicewu@Alices-MacBook-Air.local>
2023-10-10 11:49:49 -0500

    working on banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,9 +6,11 @@ export const Banner = () => {
     return (
         <section className="banner" id="home">
             <Container>
-                <Row>
-                    <Col xs={12}>
-                    
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my Portfolio</span>
+                        <h1>{'hi im web decoded'} <span className="wrap">web developer</span></h1>
+
                     </Col>
                 </Row>
             </Container>
