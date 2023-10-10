3e7c82795c2be2729790c31fe6a61a587b43050b
Alice Wu <alicewu@Alices-MacBook-Air.local>
2023-10-10 11:37:15 -0500

    initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,17 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export const Banner = () => {
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row>
+                    <Col xs={12}>
+                    
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
