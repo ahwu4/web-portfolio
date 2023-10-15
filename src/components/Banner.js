e82117b6b1030ea095616abb7cbec0ba5216610f
Alice Wu <alicewu@Alices-Air.lan>
2023-10-15 16:12:17 -0500

    completed banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,58 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+import { useState, useEffect } from "react";
 
 export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false); 
+    const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+    const [text, setText] = useState("");
+    const [delta, setDelta] = useState(300 - Math.random()*100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+        tick();
+        }, delta)
+        return () => { clearInterval(ticker)};
+    }, [text]);
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let currText = toRotate[i];
+        let newText = isDeleting ? currText.substring(0, text.length - 1) : currText.substring(0, text.length + 1);
+
+        setText(newText);
+
+        if(isDeleting){
+            setDelta(prevDelta => prevDelta/2);
+        }
+
+        if(!isDeleting && newText === currText){
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && newText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
     return (
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{'hi im web decoded'} <span className="wrap">web developer</span></h1>
-
+                        <h1>{'hi im webcoded '} <span className="wrap">{text}</span></h1>
+                        <p>Lorem ipsum is a dummy text of the printing and typesetting industry. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <button onClick={() => console.log("connect")}>Let's Connect <ArrowRightCircle size={25} /> </button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Image"/>
                     </Col>
                 </Row>
             </Container>
