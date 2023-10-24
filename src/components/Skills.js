45f429752259929a0d27365612dc4aea1387a112
Alice Wu <alicewu@Alices-MacBook-Air.local>
2023-10-24 09:50:46 -0500

    added skills carosel

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,15 @@
 import { Col, Container, Row } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
+    // sizes for different screens
     const responsive = {
         superLargeDesktop: {
-          // the naming can be any, depends on you.
           breakpoint: { max: 4000, min: 3000 },
           items: 5
         },
@@ -28,10 +32,34 @@ export const Skills = () => {
             <Container>
                 <Row>
                     <Col>
-                        
+                        <div className="skill-bx">
+                            <h2>
+                                Skills
+                            </h2>
+                            <p>Lorem Ipsum</p>
+                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                                <div className="item">
+                                    <img src={meter1} alt="meter 1" />
+                                    <h5>Web Development</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="meter 2" />
+                                    <h5>Brand Identity</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter3} alt="meter 3" />
+                                    <h5>Logo Design</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter1} alt="meter 1" />
+                                    <h5>Web Development</h5>
+                                </div>
+                            </Carousel>
+                        </div>
                     </Col>
                 </Row>
             </Container>
+            <img className="background-image-left" src={colorSharp} />
         </section>
       )
 }
